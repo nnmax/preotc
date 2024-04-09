1e1f79463683e6896ea88d0d9e293cd2468bbee6
Max <hi.max@foxmail.com>
2024-04-10 03:54:40 +0800

    update

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -15,10 +15,10 @@ export default function fetcher<ResponseData = unknown>(
   options?: FetcherOptions<ResponseData>,
 ) {
   const { disabledErrorToast, ...rest } = options || {}
-  let url = input
-  if (typeof window === 'undefined') {
-    url = NEXT_PUBLIC_API_ENDPOINT + input
-  }
+  const url = NEXT_PUBLIC_API_ENDPOINT + input
+  // if (typeof window === 'undefined') {
+  //   url = NEXT_PUBLIC_API_ENDPOINT + input
+  // }
   return fetch(url, {
     ...rest,
     headers: {
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,23 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  const url = new URL(request.url)
-  console.log(
-    '%c [ process.env.NEXT_PUBLIC_API_ENDPOINT ]-8',
-    'font-size:13px; background:pink; color:#bf2c9f;',
-    process.env.NEXT_PUBLIC_API_ENDPOINT,
-  )
-  if (url.pathname.startsWith('/pre-otc/')) {
-    const rewriteUrl = new URL(
-      url.pathname,
-      process.env.NEXT_PUBLIC_API_ENDPOINT,
-    )
-    rewriteUrl.search = url.search
-    return NextResponse.rewrite(rewriteUrl)
-  }
-}
+// export function middleware(request: NextRequest) {
+//   const url = new URL(request.url)
+//   console.log(
+//     '%c [ process.env.NEXT_PUBLIC_API_ENDPOINT ]-8',
+//     'font-size:13px; background:pink; color:#bf2c9f;',
+//     process.env.NEXT_PUBLIC_API_ENDPOINT,
+//   )
+//   // if (url.pathname.startsWith('/pre-otc/')) {
+//   //   const rewriteUrl = new URL(
+//   //     url.pathname,
+//   //     process.env.NEXT_PUBLIC_API_ENDPOINT,
+//   //   )
+//   //   rewriteUrl.search = url.search
+//   //   return NextResponse.rewrite(rewriteUrl)
+//   // }
+// }
 
-export const config = {
-  matcher: '/pre-otc/:path*',
-}
+// export const config = {
+//   matcher: '/pre-otc/:path*',
+// }
