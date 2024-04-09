142f7418029af7e7f031df3f4ef039845ac50377
Max <hi.max@foxmail.com>
2024-04-10 04:37:20 +0800

    chore: 删除 console.log

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,6 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const url = new URL(request.url)
-  console.log(
-    '%c [ process.env.NEXT_PUBLIC_API_ENDPOINT ]-8',
-    'font-size:13px; background:pink; color:#bf2c9f;',
-    process.env.NEXT_PUBLIC_API_ENDPOINT,
-  )
   if (url.pathname.startsWith('/pre-otc/')) {
     const rewriteUrl = new URL(
       url.pathname,
