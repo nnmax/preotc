0240c34ea03577cf4c16c02ec3392dfca0871bb6
Max <hi.max@foxmail.com>
2024-04-16 17:34:51 +0800

    feat: 添加 Footer 官推链接

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from 'next/font/local'
 import { Bounce, ToastContainer } from 'react-toastify'
 import AppBar from '@/components/AppBar'
 import { Providers } from '@/components/providers'
+import Footer from '@/components/Footer'
 import type { Metadata, Viewport } from 'next'
 
 const localPixelify = localFont({ src: '../fonts/pixelmix.ttf' })
@@ -28,12 +29,15 @@ export default function RootLayout({
         <Providers>
           <AppBar />
           <main
-            className={'flex min-h-[calc(100vh-64px)] justify-center px-[56px]'}
+            className={
+              'flex min-h-[calc(100vh-64px-64px)] justify-center px-14 pb-6'
+            }
           >
             <div className={'flex w-full max-w-[1684px] flex-col'}>
               {children}
             </div>
           </main>
+          <Footer />
           <ToastContainer draggable transition={Bounce} />
         </Providers>
       </body>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import WhiteLogo from '@/images/white-logo.svg'
+import XFilled from '@/images/x-filled.svg'
+
+export default function Footer() {
+  return (
+    <footer className={'flex h-16 w-full items-center bg-black px-14'}>
+      <Link
+        href={'https://twitter.com/preotc_xyz'}
+        className={'flex items-center gap-2'}
+      >
+        <Image src={WhiteLogo} alt={'PreOTC'} />
+        <div
+          className={
+            'flex h-6 w-6 items-center justify-center rounded border border-white/50'
+          }
+        >
+          <Image src={XFilled} alt={'X'} />
+        </div>
+      </Link>
+    </footer>
+  )
+}
